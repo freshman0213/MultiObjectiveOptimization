f18f562819499180e60ebb0a26ab124602a83244
freshman0213 <b06902043@csie.ntu.edu.tw>
2022-11-08 20:21:24 -0800

    Models

--- multi_task/models/multi_lenet.py
+++ multi_task/models/multi_lenet.py
@@ -39,14 +39,7 @@ class MultiFilmLeNetR(nn.Module):
         self.film3_task1 = FiLM(input_shape = (1, 50))
         self.film3_task2 = FiLM(input_shape = (1, 50))
 
-    def dropout2dwithmask(self, x, mask):
-        channel_size = x.shape[1]
-        if mask is None:
-            mask = Variable(torch.bernoulli(torch.ones(1,channel_size,1,1)*0.5).to(DEVICE))
-        mask = mask.expand(x.shape)
-        return mask
-
-    def forward(self, x, mask):
+    def forward(self, x):
         x = self.conv1(x)
         x1 = self.film1_task1(x)
         x2 = self.film1_task2(x)
@@ -69,7 +62,7 @@ class MultiFilmLeNetR(nn.Module):
         x2 = self.film3_task2(x2)
         x1 = F.relu(x1)
         x2 = F.relu(x2)
-        return x1, x2, mask
+        return x1, x2
 
 
 class MultiLeNetR(nn.Module):
@@ -80,14 +73,7 @@ class MultiLeNetR(nn.Module):
         self.conv2_drop = nn.Dropout2d(p=params['dropout_rate'])
         self.fc = nn.Linear(320, 50)
 
-    def dropout2dwithmask(self, x, mask):
-        channel_size = x.shape[1]
-        if mask is None:
-            mask = Variable(torch.bernoulli(torch.ones(1,channel_size,1,1)*0.5).to(DEVICE))
-        mask = mask.expand(x.shape)
-        return mask
-
-    def forward(self, x, mask):
+    def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = self.conv2(x)
         if self.training:
@@ -95,19 +81,19 @@ class MultiLeNetR(nn.Module):
         x = F.relu(F.max_pool2d(x, 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc(x))
-        return x, mask
+        return x
 
 class MultiLeNetO(nn.Module):
-    def __init__(self):
+    def __init__(self, params):
         super(MultiLeNetO, self).__init__()
         self.fc1 = nn.Linear(50, 50)
+        self.fc1_drop = nn.Dropout2d(p=params['dropout_rate'])
         self.fc2 = nn.Linear(50, 10)
     
-    def forward(self, x, mask):
-        x = F.relu(self.fc1(x))
-        if mask is None:
-            mask = Variable(torch.bernoulli(x.data.new(x.data.size()).fill_(0.5)))
+    def forward(self, x):
+        x = self.fc1(x)
         if self.training:
-            x = x*mask
+            x = self.fc1_drop(x)
+        x = F.relu(x)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1), mask
+        return F.log_softmax(x, dim=1)
