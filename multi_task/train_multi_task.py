84717212ba339047e5d064dc74f350fb3fd862a5
Ubuntu <tzwmo@tzwmo.qef2k0h40zfejmfyeglsvjh3me.cx.internal.cloudapp.net>
2022-11-11 23:03:11 +0000

    more results

--- multi_task/train_multi_task.py
+++ multi_task/train_multi_task.py
@@ -207,7 +207,7 @@ def train_multi_task(params):
                     metric_results = metric[t].get_result()
                     for metric_key in metric_results:
                         state['metric_{}_{}'.format(metric_key, t)] = metric_results[metric_key]
-                        # writer.add_scalar('metric_{}_{}'.format(metric_key, t), metric_results[metric_key], n_iter) 
+                        writer.add_scalar('metric_{}_{}'.format(metric_key, t), metric_results[metric_key], n_iter) 
                     metric[t].reset()
                 state['testing_loss'] = tot_loss['all']/len(test_dst) 
 
