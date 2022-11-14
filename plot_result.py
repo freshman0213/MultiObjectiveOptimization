314cb130a7863251ddc6179bb11d2fd09b0b30de
annie9032 <Hichen2924@gmail.com>
2022-11-13 21:16:29 -0800

    handle multiple models in plot_result.py and upload plot

--- plot_result.py
+++ plot_result.py
@@ -16,40 +16,52 @@ def pareto_dominate(a, b):
 
 # find npy files in the folder
 result_file_list = []
-for file in os.listdir("../"): 
+for file in os.listdir("./"): 
     filename, extension = os.path.splitext(file)
     if extension == '.npy':
-        result_file_list.append("../" + file)
+        result_file_list.append("./" + file)
 
 # load npy files
-pareto_frontier = []
-pareto_dominated = []
 
-all_results = []
+all_results = {}
+datasets = []
+pareto_frontier = {}
+pareto_dominated = {}
 for file in result_file_list: 
     results = np.load(file, allow_pickle=True)
-    all_results += [[pt['L'], pt['R']] for pt in results]
+    file_split = file.split('|')
+    for s in file_split: 
+        if 'dataset=' in s: 
+            dataset = s[9:]
+    if dataset in all_results: 
+        all_results[dataset] += [[pt['L'], pt['R']] for pt in results]
+    else:
+        datasets.append(dataset)
+        all_results[dataset] = [[pt['L'], pt['R']] for pt in results]
+        pareto_frontier[dataset] = []
+        pareto_dominated[dataset] = []
 
-pareto_frontier = []
-for i, target in enumerate(all_results): 
-    non_dominated = True
-    for j, temp in enumerate(all_results): 
-        if i != j and pareto_dominate(temp, target): 
-            non_dominated = False
-    if non_dominated: 
-        pareto_frontier.append(target)
-    else: 
-        pareto_dominated.append(target)
+print(all_results)
+for dataset in datasets:
+    for i, target in enumerate(all_results[dataset]): 
+        non_dominated = True
+        for j, temp in enumerate(all_results[dataset]): 
+            if i != j and pareto_dominate(temp, target): 
+                non_dominated = False
+        if non_dominated: 
+            pareto_frontier[dataset].append(target)
+        else: 
+            pareto_dominated[dataset].append(target)
 
-pareto_frontier = np.array(pareto_frontier)
-pareto_dominated = np.array(pareto_dominated)
+colors = ['tab:blue', 'tab:orange']
+for i, dataset in enumerate(datasets):
+    frontier = np.array(pareto_frontier[dataset])
+    other = np.array(pareto_dominated[dataset])
+    sort_indices = np.argsort(frontier[:, 0])
+    frontier = frontier[sort_indices]
+    plt.scatter(other[:, 0], other[:, 1], color=colors[i], marker='.')
+    plt.plot(frontier[:, 0], frontier[:, 1], color=colors[i], marker='.', label=dataset)
 
-
-sort_indices = np.argsort(pareto_frontier[:, 0])
-pareto_frontier = pareto_frontier[sort_indices]
-
-plt.scatter(pareto_dominated[:, 0], pareto_dominated[:, 1], label='pareto_dominated', color='r', marker='.')
-plt.plot(pareto_frontier[:, 0], pareto_frontier[:, 1], label='pareto_frontier', color='r', marker='.')
 plt.legend()
 plt.show()
     
